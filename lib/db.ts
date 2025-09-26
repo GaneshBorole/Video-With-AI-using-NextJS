e1aaa699eb12383e620a953fc702616f516a645d
ganeshborole <ganeshborole73@gmail.com>
2025-09-26 10:25:02 +0530

    database connection is done

---
+++ lib/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { buffer } from "stream/consumers";
+const MONGODB_URI=process.env.MONGODB_URI !;
+if(!MONGODB_URI){
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+let cached=global.mongoose
+if(!cached){
+    cached=global.mongoose={conn:null,promise:null};
+}
+export async function connectToDB(){
+    if(cached.conn){
+        return cached.conn;
+    }
+    if(!cached.promise){
+        const opts={
+            bufferCommands:true,
+            maxPoolSize:10,
+        }
+        mongoose
+        .connect(MONGODB_URI)
+        .then(()=>mongoose.connection)
+}
+try {
+    cached.conn=await cached.promise;
+} catch (error) {
+    
+}
+return cached.conn;
+}
