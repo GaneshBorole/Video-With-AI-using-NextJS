59f6af1b93598f0e1f38e8d1ff37e2c781669a71
ganeshborole <ganeshborole73@gmail.com>
2025-09-30 13:20:49 +0530

    backend part is done

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import { useState } from 'react';   
+
+function RegisterPage() {
+    const [email, setEmail] = useState("");
+  return (
+
+    <div>Welcome to register page</div>
+  )
+}
+
+export default RegisterPage
